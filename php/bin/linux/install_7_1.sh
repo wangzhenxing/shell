9ec37218bad5ab383ffed8cc34bafe129c4cbef1
cloudsky <wzx1987310@gmail.com>
2019-09-03 22:04:20 +0800

    add php install shell

---
+++ php/bin/linux/install_7_1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+yum remove php* -y
+
+# centos 7.x
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+# centos 6.x
+#rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+
+yum install php71w -y
+yum install php71w* --skip-broken -y
+
+php -v
+
+service php-fpm start
