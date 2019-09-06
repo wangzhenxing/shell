f1ed04bde7530a02c690da233aea35d94d391dad
wangzhenxing <wzx1987310@gmail.com>
2019-09-07 07:55:48 +0800

    add lszrz shell

--- mac_common/bin/lszrz.sh
+++ mac_common/bin/lszrz.sh
@@ -7,10 +7,19 @@ cp ../etc/iterm2-send-zmodem.sh /usr/local/bin
 cp ../etc/iterm2-recv-zmodem.sh /usr/local/bin
 
 cd /usr/local/bin
-chmod +x iterm2-send-zmodem.sh 
-chmod +x iterm2-recv-zmodem.sh
+chmod 777 iterm2-send-zmodem.sh 
+chmod 777 iterm2-recv-zmodem.sh
 
 # 界面增加trigger
-#Preferences -> Profiles -> Advanced -> Default -> Triggers -> Edit 添加
-#\*\*\*\*B0100 RunSilent Coprocess... /usr/local/bin/iterm2-send-zmodem.sh
-#\*\*B00000000000000 RunSilent Coprocess... /usr/local/bin/iterm2-recv-zmodem.sh
+#设置Iterm2的Tirgger特性，profiles->default->editProfiles->Advanced中的Tirgger
+#添加两条trigger，分别设置 Regular expression，Action，Parameters，Instant如下：
+#1.第一条
+#Regular expression: rz waiting to receive.\*\*B0100
+#Action: Run Silent Coprocess
+#Parameters: /usr/local/bin/iterm2-send-zmodem.sh
+#Instant: checked
+#2.第二条
+#Regular expression: \*\*B00000000000000
+#Action: Run Silent Coprocess
+#Parameters: /usr/local/bin/iterm2-recv-zmodem.sh
+#Instant: checked
