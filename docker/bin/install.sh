629c1199b0977324107f3635552b58d51e48e169
wangzhenxing <wzx1987310@gmail.com>
2019-09-12 17:21:01 +0800

    update docker

--- docker/bin/install.sh
+++ docker/bin/install.sh
@@ -23,6 +23,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 # 安装docker composer
-curl -L https://get.daocloud.io/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
