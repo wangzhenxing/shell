2571a03f1689dd2783e7dce87ffa145138fb8a05
wangzhenxing <wzx1987310@gmail.com>
2019-09-18 00:41:03 +0800

    add mongo

--- kong/centos/install_konga.sh
+++ kong/centos/install_konga.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+set -e
+set -x
+
 docker pull pantsel/konga
 
+docker network rm kong-network
 docker network create kong-network
 
-docker run -p 1337:1337 \
+docker rm konga
+# DB_HOST不能用localhost，创建mongo的时候bindip用本机内网ip
+# DB_HOST用内网ip，不能用localhost
+docker run -d -p 1337:1337 \
     --network kong-network \
-    --name konga-new \
+    --name konga \
     -e "NODE_ENV=production" \
     -e "TOKEN_SECRET=KongSecret" \
+    -e "DB_ADAPTER=mongo" \
+    -e "DB_HOST=172.16.8.113" \
+    -e "DB_PORT=27017" \
+    -e "DB_USER=admin" \
+    -e "DB_PASSWORD=123456" \
+    -e "DB_DATABASE=konga" \
     pantsel/konga
---
+++ mongo/bin/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo '[mongodb-org-4.2]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb-org-4.2.repo
+
+sudo yum install -y mongodb-org
+
+systemctl start mongod.service
+
+mongo
