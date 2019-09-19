73a66203b4fcd15d0a1298a9d0784d4c1af73630
wangzhenxing <wzx1987310@gmail.com>
2019-09-19 08:38:16 +0800

    update cassandra

--- cassandra/bin/install_centos.sh
+++ cassandra/bin/install_centos.sh
@@ -12,7 +12,7 @@ baseurl=https://www.apache.org/dist/cassandra/redhat/311x/
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://www.apache.org/dist/cassandra/KEYS' > /etc/yum.repos.d/cassandra.repo
-    sudo yum install cassandra
+    sudo yum install cassandra -y
 }
 
 # function start
