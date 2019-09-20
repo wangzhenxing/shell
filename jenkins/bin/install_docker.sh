bee67b8b9265e61a6204082ccd95959d5964b8f0
wangzhenxing <wzx1987310@gmail.com>
2019-09-20 17:56:34 +0800

    add jenkins

---
+++ jenkins/bin/install_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -x
+
+docker pull jenkins/jenkins
+
+docker run \
+    -u root \
+    --rm \
+    -d \
+    -p 8300:8080 \
+    -p 50000:50000 \
+    -v jenkins-data:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    jenkins/jenkins
