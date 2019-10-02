98aa60fd6b1c6f4581005ee20dbd118c1b2b6f9a
wangzhenxing <wzx1987310@gmail.com>
2019-10-02 10:18:31 +0800

    update jenkins with docker

--- jenkins/bin/install_docker.sh
+++ jenkins/bin/install_docker.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 set -e -x
 
-docker pull jenkins/jenkins
+## 经典版本
+classtic() {
+    docker pull jenkins/jenkins
 
-docker run \
-    -u root \
-    --rm \
-    -d \
-    -p 8300:8080 \
-    -p 50000:50000 \
-    -v /newdata/jenkins-data:/var/jenkins_home \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    jenkins/jenkins
+    docker run \
+        -u root \
+        --rm \
+        -d \
+        -p 8300:8080 \
+        -p 50000:50000 \
+        -v /newdata/jenkins-data:/var/jenkins_home \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        jenkins/jenkins
+}
+
+## blue ocean版本
+blueOcean() {
+    docker pull jenkinsci/blueocean
+
+    docker run \
+        -u root \
+        --rm \
+        -d \
+        -p 8300:8080 \
+        -p 50000:50000 \
+        -v jenkins-data:/var/jenkins_home \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        jenkinsci/blueocean 
+}
+
+##### main logic #####
+blueOcean
